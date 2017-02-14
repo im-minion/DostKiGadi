517366e2b3db3b42d0655fec11b651f4fe69b6a0
vpm235 <vaibhavminiyar@gmail.com>
2017-02-14 19:13:32 +0530

    first commit

---
+++ app/src/main/java/com/example/vinayak/mapsapi/BookActivity.java
@@ -0,0 +1,59 @@
+package com.example.vinayak.mapsapi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class BookActivity extends AppCompatActivity {
+
+    EditText noOfFriends;
+    TextView noOfCarsAval;
+    Button BookButton;
+    int cars;
+    int friends;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+        noOfFriends = (EditText) findViewById(R.id.editText);
+        noOfCarsAval = (TextView) findViewById(R.id.noOfcar);
+        BookButton = (Button) findViewById(R.id.btn);
+
+        cars = Integer.parseInt(String.valueOf(noOfCarsAval.getText()));
+        Log.w("CAR VALE IS : ", String.valueOf(cars));//working
+        //friends = Integer.parseInt(String.valueOf(noOfFriends.getText()));
+
+
+    }
+
+    public void carBooked(View view) {
+        friends = Integer.parseInt(String.valueOf(noOfFriends.getText()));
+        if (friends >= 1 && friends <= 2) {
+            if (cars == 0) {
+                Toast.makeText(this,"Oooo!Sorry No Cars are available", Toast.LENGTH_SHORT).show();
+            } else {
+                cars--;
+                String temp = String.valueOf(cars);
+                noOfCarsAval.setText(temp);
+                Toast.makeText(this,"Hoola!Car is Booked!Wait For it!!", Toast.LENGTH_SHORT).show();
+                noOfFriends.setText("");
+            }
+        } else {
+
+            if (friends<=0){
+                Toast.makeText(this, "Friends can't be 0 or less", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "Friends must be less than 3", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+    }
+}
--- app/src/main/java/com/example/vinayak/mapsapi/MainActivity.java
+++ app/src/main/java/com/example/vinayak/mapsapi/MainActivity.java
@@ -1,34 +1,46 @@
 package com.example.vinayak.mapsapi;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    GoogleApiClient mGoogleApiClient;
-    private Location mLastLocation;
+    GoogleApiClient mGoogleApiClient;   //from GoogleApiClient
+    private Location mLastLocation;     //
     TextView mLatitudeText;
     TextView mLongitudeText;
-
+    private Geocoder geocoder;          //geocoder to gettng the addres from latitude and longitude
+    List<Address> addresses;            //for storing the addresslines street city state
+    Double lati, longi;
+    String location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mLatitudeText = (TextView)findViewById(R.id.textView2);
-        mLongitudeText = (TextView)findViewById(R.id.textView3);
+        mLongitudeText = (TextView) findViewById(R.id.textView3);
+
+        geocoder = new Geocoder(this, Locale.getDefault());
 
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -37,7 +49,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                     .addApi(LocationServices.API)
                     .build();
         }
-
     }
 
     protected void onStart() {
@@ -65,10 +76,28 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
         if (mLastLocation != null) {
-            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
-            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+            //mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+            //mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+            //Toast.makeText(MainActivity.this, "Latitude" + mLastLocation.getLatitude(), Toast.LENGTH_LONG).show();
+            //Toast.makeText(MainActivity.this, "Latitude" + mLastLocation.getLongitude(), Toast.LENGTH_LONG).show();
+            lati = mLastLocation.getLatitude();
+            longi = mLastLocation.getLongitude();
+            //String link="https://maps.googleapis.com/maps/api/geocode/json?latlng="+lati+","+longi+"&key=AIzaSyAFzdySfoQ5vaZ8tnVCOoAYSApUhf0hP7I";
 
-            Toast.makeText(MainActivity.this,"Latitude"+mLastLocation.getLatitude(),Toast.LENGTH_LONG).show();
+            try {
+                addresses = geocoder.getFromLocation(lati, longi, 2);//passing lati and longi (double type) values to get adress
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String address = addresses.get(0).getAddressLine(0);//get the street
+            String city = addresses.get(0).getLocality();       //get city name
+            String state = addresses.get(0).getAdminArea();     //get state name
+
+            location = address + " " + city + " " + state; //this will conacat street, city and state
+
+            //Toast.makeText(MainActivity.this,"Location is:"+address,Toast.LENGTH_SHORT).show();
+            mLongitudeText.setText(location);
+
 
         }
     }
@@ -80,6 +109,24 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    public void shareLocation(View view) {
+        //give the intent to gmail
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));                           // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "My Location");           //subject to gmail
+        intent.putExtra(Intent.EXTRA_TEXT, "My Location is :" + location);//message content to gmail
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+    }
+
+    public void newActivity(View view) {
+
+        startActivity(new Intent(MainActivity.this, BookActivity.class));
 
     }
 }
